ffeb545aa183f5491c271c51d80843076c4dee0c
Leon <yanglyu.leon.7@gmail.com>
2018-05-08 14:39:48 -0700

    demo: Working with Serializer Settings
    Module 3.12

--- CityInfo.API/CityInfo.API/Startup.cs
+++ CityInfo.API/CityInfo.API/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -11,7 +12,16 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(o =>
+                {
+                    if (o.SerializerSettings.ContractResolver != null)
+                    {
+                        var castedResolver = o.SerializerSettings.ContractResolver
+                            as DefaultContractResolver;
+                        castedResolver.NamingStrategy = null;
+                    }
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
