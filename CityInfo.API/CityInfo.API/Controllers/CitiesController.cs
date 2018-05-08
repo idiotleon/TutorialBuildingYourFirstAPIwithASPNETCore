47a9a753ba4b351bba3f6b82443685eb6e62b673
Leon <yanglyu.leon.7@gmail.com>
2018-05-08 13:20:16 -0700

    demo: Improving the Architecture
    Module 3.9

--- CityInfo.API/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/CityInfo.API/Controllers/CitiesController.cs
@@ -6,14 +6,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("api/cities")]
     public class CitiesController : Controller
     {
+        [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>() {
-                   new { id = 1, Name="New York City"},
-                   new { id = 2, Name="Antwerp"}
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+                );
         }
     }
 }
