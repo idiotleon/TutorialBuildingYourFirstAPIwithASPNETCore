cdab9c8fbc4f90919a1dee433340fdd95a029d31
Leon <yanglyu.leon.7@gmail.com>
2018-05-14 12:06:15 -0700

    demo: Working with Configuration Files
    Module 5.5

--- CityInfo.API/CityInfo.API/Startup.cs
+++ CityInfo.API/CityInfo.API/Startup.cs
@@ -6,11 +6,19 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using NLog.Extensions.Logging;
 using CityInfo.API.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
